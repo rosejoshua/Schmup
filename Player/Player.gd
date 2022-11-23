d54c461034c6e59312467ef4c4f916554a1ae24b
jr <jlr007@protonmail.com>
2022-11-23 14:40:08 -0600

    first commit

---
+++ Player/Player.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+var plBullet := preload("res://Bullet/Bullet.tscn")
+
+onready var animatedSprite := $AnimatedSprite
+onready var firingPositions := $FiringPositions
+onready var fireDelayTimer := $FireDelayTimer
+
+export var speed: float = 100
+export var fireDelay: float = 0.1
+var vel := Vector2(0,0)
+
+func _process(delta):
+	if vel.x < 0:
+		animatedSprite.play("left")
+	elif vel.x > 0:
+		animatedSprite.play("right")
+	else:
+		animatedSprite.play("straight")
+
+func _physics_process(delta):
+	var dirVec := Vector2(0,0)
+
+	if Input.is_action_pressed("move_left"):
+		dirVec.x = -1
+	elif Input.is_action_pressed("move_right"):
+		dirVec.x = 1
+	if Input.is_action_pressed("move_up"):
+		dirVec.y = -1
+	elif Input.is_action_pressed("move_down"):
+		dirVec.y = 1
+
+	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
+		fireDelayTimer.start(fireDelay)
+		for child in firingPositions.get_children():
+			var bullet := plBullet.instance()
+			bullet.global_position = child.global_position
+			get_tree().current_scene.add_child(bullet)
+
+
+	vel = dirVec.normalized() * speed
+	position += vel * delta
+
+	#clamping
+	var viewRect := get_viewport_rect()
+	position.x = clamp(position.x, viewRect.position.x, viewRect.end.x)
+	position.y = clamp(position.y, viewRect.position.y, viewRect.end.y)
